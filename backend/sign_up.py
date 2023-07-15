e6c1ff5f5db5879808388d8001baee602552b48e
Matija Zarić <matijazaric9@gmail.com>
2023-07-15 16:17:48 +0200

    Added sign up

---
+++ backend/sign_up.py
@@ -0,0 +1,108 @@
+import json
+import boto3
+import psycopg2
+from botocore.exceptions import ClientError
+
+secrets_manager_client = boto3.client('secretsmanager', region_name='eu-north-1')
+cognito_client = boto3.client('cognito-idp', region_name='eu-north-1')
+
+class User:
+    def __init__(self, email, given_name, family_name, address, phone_number, birthdate):
+        self.email = email
+        self.given_name = given_name
+        self.family_name = family_name
+        self.address = address
+        self.phone_number = phone_number
+        self.birthdate = birthdate    
+
+def get_secrets_manager_credentials(secret_name):
+    try:
+        response = secrets_manager_client.get_secret_value(SecretId=secret_name)
+        secret_string = response['SecretString']
+        return secret_string
+    except Exception as e:
+        return None
+    
+def get_connection(secret_string):
+    credentials = eval(secret_string)
+    conn = psycopg2.connect(
+        host=credentials['host'],
+        port=credentials['port'],
+        dbname=credentials['dbname'],
+        user=credentials['username'],
+        password=credentials['password']
+    )
+    return conn
+
+def write_user_to_database(conn, user):
+    try:
+        cursor = conn.cursor()
+        insert_query = "INSERT INTO users (email, given_name, family_name, address, phone_number, birthdate) VALUES (%s, %s, %s, %s, %s, %s);"
+        cursor.execute(insert_query, (user.email, user.given_name, user.family_name, user.address, user.phone_number, user.birthdate,))
+        conn.commit()
+        cursor.close()
+        return True
+    except Exception as e:
+        print(f"Error writing object to database: {str(e)}")
+        return False
+    
+def singup_user(event, client_id):
+    try:
+        cognito_client.sign_up(
+            ClientId=client_id,
+            Username=event['username'],
+            Password=event['password'],
+            UserAttributes=[
+                {
+                    'Name': 'given_name',
+                    'Value': event['given_name']
+                },
+                {
+                    'Name': 'family_name',
+                    'Value': event['family_name']
+                },
+                {
+                    'Name': 'email',
+                    'Value': event['username']
+                },                
+                {
+                    'Name': 'birthdate',
+                    'Value': event['birthdate']
+                },
+                {
+                    'Name': 'phone_number',
+                    'Value': event['phone_number']
+                },
+                {
+                    'Name': 'address',
+                    'Value': event['address']
+                }
+            ]
+        )
+        return User(event['username'], event['given_name'], event['family_name'], event['address'], event['phone_number'], event['birthdate'])
+    except ClientError as e:
+        return e
+    
+def lambda_handler(event, context):
+    secret_string_cognito = eval(get_secrets_manager_credentials("dev/WenShop/UserPoolCred"))
+    secret_string_db = get_secrets_manager_credentials("dev/WebShop/PostgreSQL")
+    if secret_string_cognito and secret_string_db:
+        response = singup_user(event, secret_string_cognito['ClientID'])
+        if not isinstance(response, User):
+            return {
+                'statusCode': 500,
+                'body': json.dumps(f"Error writing object to database: {str(response)}")
+            }
+        conn = get_connection(secret_string_db)
+        write_user_to_database(conn, response)
+        conn.close()
+        return {
+            'statusCode': 200,
+            'body': json.dumps('You signed up successfully.')
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error getting credentials')
+        }
+            
