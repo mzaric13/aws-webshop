5db42e9da3c658b2e12735fc6ac82843f534aaf6
Matija Zarić <matijazaric9@gmail.com>
2023-07-18 15:42:25 +0200

    Created navbar for home page

---
+++ web-shop-front/src/components/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../organisms/Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+};
+
+export default HomePage;
